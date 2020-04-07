d49df49879fdb8283472a627ec6f21c01ec535f0
nealhaddaway <43783900+nealhaddaway@users.noreply.github.com>
2020-04-07 09:54:11 +0200

    First upload

---
+++ pubmed_result.R
@@ -0,0 +1,10 @@
+covid<-read.csv(file.choose())
+str(covid)
+attach(covid)
+library(ggplot2)
+
+#histogram of COVID research found on 7th April 2020 in PUBMED (all field search for "COVID")
+ggplot(data=covid,aes(x=(week-1)))+
+  geom_histogram(stat="count",binwidth=1,fill="#aec6cf")+
+  xlab("Week of 2020")+
+  ylab("Record count")
