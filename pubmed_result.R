d98797315b45526c0d36c4c0ead0e8b9d5b3258f
nealhaddaway <43783900+nealhaddaway@users.noreply.github.com>
2020-04-07 10:17:29 +0200

    Correction to week bins

--- pubmed_result.R
+++ pubmed_result.R
@@ -4,7 +4,7 @@ attach(covid)
 library(ggplot2)
 
 #histogram of COVID research found on 7th April 2020 in PUBMED (all field search for "COVID")
-ggplot(data=covid,aes(x=(week-1)))+
+ggplot(data=covid,aes(x=week))+
   geom_histogram(stat="count",binwidth=1,fill="#aec6cf")+
   xlab("Week of 2020")+
   ylab("Record count")
