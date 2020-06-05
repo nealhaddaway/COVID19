34960050cd73984b06a4c104f9291b8256297203
nealhaddaway <43783900+nealhaddaway@users.noreply.github.com>
2020-06-05 13:38:21 +0200

    Search update 5th June 2020 for article revision

--- pubmed_result.R
+++ pubmed_result.R
@@ -3,7 +3,7 @@ str(covid)
 attach(covid)
 library(ggplot2)
 
-#histogram of COVID research found on 7th April 2020 in PUBMED (all field search for "COVID")
+#histogram of COVID research found on 5th June 2020 in PUBMED (all field search for "COVID-19" OR "nCoV" OR "2019 novel coronavirus" OR "2019-nCoV" OR "SARS-CoV-2": 19,274 hits, restricted to creation dates in 2020 = 19,260 records)
 ggplot(data=covid,aes(x=week))+
   geom_histogram(stat="count",binwidth=1,fill="#aec6cf")+
   xlab("Week of 2020")+
